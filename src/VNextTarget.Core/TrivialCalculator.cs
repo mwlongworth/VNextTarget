4fb6950d903ea02c3357ec45cca2fbfbecc2bc54
Matthew Longworth <matt.longworth@level-b.co.uk>
2015-07-27 19:04:51 +0100

    Initial commit

---
+++ src/VNextTarget.Core/TrivialCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VNextTarget.Core
+{
+    public class TrivialCalculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ src/VNextTarget.Tests/TrivialCalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VNextTarget.Tests
+{
+    using NUnit.Framework;
+
+    using VNextTarget.Core;
+
+    [TestFixture]
+    public class TrivialCalculatorTests
+    {
+        [Test]
+        public void Add_WhenInputsAreCorrect_ThenAnswerIsProduced()
+        {
+            var subject = new TrivialCalculator();
+
+            var result = subject.Add(2, 5);
+
+            Assert.That(result, Is.EqualTo(7));
+        }
+    }
+}
